f864d32c4a34de37a91ab5a84d4366ce897f2736
11m245 <sivaraj2siva@gmail.com>
2023-01-20 13:46:51 +0530

    update url

--- src/App.js
+++ src/App.js
@@ -18,8 +18,8 @@ export const apiContext = createContext();
 function App() {
   const serverApi = "http://localhost:4000";
   const clientURL = "http://localhost:3000";
-  // const serverApi = "https://url-short-backend-two.vercel.app";
-  // const clientURL = "https://curious-llama-201728.netlify.app/";
+  // const serverApi = "https://pizza-delivery-backend.vercel.app";
+  // const clientURL = "https://candid-blancmange-22f08a.netlify.app";
   const contextObj = {
     serverApi: serverApi,
     clientURL: clientURL,
